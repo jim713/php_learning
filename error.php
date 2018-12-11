d9d1d873c41c02ba5dd19f9f9983316588b73297
qiwei <18410259711@163.com>
2018-12-11 16:12:24 +0800

    php学习
    php学习

--- error.php
+++ error.php
@@ -1,6 +1,21 @@
 <?php
-if (!file_exists("welcome.txt")) {
-    die("文件不存在");
-} else {
-    $file = fopen("welcome.txt", "r");
+// if (!file_exists("welcome.txt")) {
+//     die("文件不存在");
+// } else {
+//     $file = fopen("welcome.txt", "r");
+// }
+function checkNum($number)
+{
+    if ($number > 1) {
+        throw new Exception("传入数字应该 <1");
+    }
+    return true;
+}
+
+try {
+    checkNum(2);
+    echo "正常";
+} catch (Exception $e) {
+    echo $e->getMessage();
+
 }
